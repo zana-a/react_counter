5b3910df57b49a26242f5edffa46e9056c1a27a7
zana-a <zana200.a@gmail.com>
2021-09-30 18:57:27 +0100

    add handle functions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,13 +15,29 @@ class Counter extends Component {
     });
   }
 
+  handleDecrement = () => {
+    const { value } = this.state;
+    this.setState({
+      value: value === 0 ? 0 : value - 1,
+    });
+  }
+
+  counterClassName = () => {
+    const { value } = this.state;
+    let className = 'badge badge-';
+    if (value === 0) className += 'red';
+    else className += 'blue';
+    return className;
+  }
+
   render() {
     const { value } = this.state;
 
     return (
       <main className="container">
-        <span className="badge badge-blue">{value}</span>
+        <span className={this.counterClassName()}>{value}</span>
         <button type="button" onClick={this.handleIncrement}>Increment</button>
+        <button type="button" onClick={this.handleDecrement}>Decrement</button>
       </main>
     );
   }
