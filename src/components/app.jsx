5496b3c4cc48e0f93a8478cb57ae4e3397c19946
zana-a <zana200.a@gmail.com>
2021-10-01 14:35:53 +0100

    add fab

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,16 +5,91 @@ import { CssBaseline, Container, Fab, Box } from "@mui/material";
 import { Add } from "@mui/icons-material";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counters: [{ id: 1, value: 0 }],
+    };
+  }
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value += 1;
+    this.setState({
+      counters,
+    });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    if (!(counters[index].value === 0)) counters[index].value -= 1;
+    this.setState({
+      counters,
+    });
+  };
+
+  handleReset = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value = 0;
+    this.setState({
+      counters,
+    });
+  };
+
+  handleResetAll = () => {
+    const counters = [...this.state.counters];
+    counters.map((counter) => (counter.value = 0));
+    this.setState({
+      counters,
+    });
+  };
+
+  handleDeleteAll = () => {
+    let counters = [...this.state.counters];
+    counters = [];
+    this.setState({
+      counters,
+    });
+  };
+
+  handleDelete = (other) => {
+    let counters = [...this.state.counters];
+    counters = counters.filter((counter) => counter.id !== other.id);
+    this.setState({
+      counters,
+    });
+  };
+
+  handleNew = () => {
+    let counters = [...this.state.counters];
+    counters.push({ id: counters.length + 1, value: 0 });
+    this.setState({
+      counters,
+    });
+  };
+
   render() {
     return (
       <div>
         <CssBaseline />
         <Navbar />
         <Container maxWidth="md">
-          <Counters />
+          <Counters
+            onNew={this.handleNew}
+            onResetAll={this.handleResetAll}
+            onReset={this.handleReset}
+            onDeleteAll={this.handleDeleteAll}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            data={this.state}
+          />
         </Container>
         <Box style={{ position: "fixed", right: 0, bottom: 0 }} padding="1rem">
-          <Fab color="primary">
+          <Fab color="primary" onClick={() => this.handleNew()}>
             <Add />
           </Fab>
         </Box>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,93 +4,37 @@ import { Button } from "@mui/material";
 import { ClearAll } from "@mui/icons-material";
 
 class Counters extends Component {
-  constructor() {
-    super();
-    this.state = {
-      counters: [{ id: 1, value: 0 }],
-    };
-  }
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index].value += 1;
-    this.setState({
-      counters,
-    });
-  };
-
-  handleDecrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    if (!(counters[index].value === 0)) counters[index].value -= 1;
-    this.setState({
-      counters,
-    });
-  };
-
-  handleReset = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index].value = 0;
-    this.setState({
-      counters,
-    });
-  };
-
-  handleResetAll = () => {
-    const counters = [...this.state.counters];
-    counters.map((counter) => (counter.value = 0));
-    this.setState({
-      counters,
-    });
-  };
-
-  handleDeleteAll = () => {
-    let counters = [...this.state.counters];
-    counters = [];
-    this.setState({
-      counters,
-    });
-  };
-
-  handleDelete = (other) => {
-    let counters = [...this.state.counters];
-    counters = counters.filter((counter) => counter.id !== other.id);
-    this.setState({
-      counters,
-    });
-  };
-
-  handleNew = () => {
-    let counters = [...this.state.counters];
-    counters.push({ id: counters.length + 1, value: 0 });
-    this.setState({
-      counters,
-    });
-  };
-
   render = () => {
-    const { counters } = this.state;
+    const {
+      onIncrement,
+      onDecrement,
+      onReset,
+      onDelete,
+      onResetAll,
+      onDeleteAll,
+      onNew,
+      data,
+    } = this.props;
+
     return (
       <main className="container">
-        {counters.map((data) => (
+        {data.counters.map((counter) => (
           <Counter
-            key={data.id}
-            data={data}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onReset={this.handleReset}
-            onDelete={this.handleDelete}
+            key={counter.id}
+            data={counter}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+            onReset={onReset}
+            onDelete={onDelete}
           />
         ))}
-        <Button variant="contained" onClick={this.handleResetAll}>
+        <Button variant="contained" onClick={() => onResetAll()}>
           Reset All
         </Button>
-        <Button variant="contained" onClick={this.handleDeleteAll}>
+        <Button variant="contained" onClick={() => onDeleteAll()}>
           <ClearAll />
         </Button>
-        <Button variant="contained" onClick={this.handleNew}>
+        <Button variant="contained" onClick={() => onNew()}>
           New
         </Button>
       </main>
